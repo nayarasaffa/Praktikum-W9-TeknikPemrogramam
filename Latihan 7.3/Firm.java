177db9f1576c389181cc585a0641d512fd8d2ca1
Nayara Saffa <95131818+nayarasaffa@users.noreply.github.com>
2022-04-07 23:19:45 +0700

    Add files via upload

---
+++ Latihan 7.3/Firm.java
@@ -0,0 +1,9 @@
+// Demonstrates polymorphism via inheritance
+
+public class Firm {
+	// Creates a staff of employees for a firm and pays them
+	public static void main (String[] args) {
+		Staff personel = new Staff();
+		personel.payday();
+	}
+}
